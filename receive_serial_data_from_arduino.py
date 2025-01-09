5af12ed4ef15ca2990ce80af28e5e2cf5745b523
Fazida Ashraf Puthiyapurayil <FazidaAshraf_Puthiyapurayil@student.uml.edu>
2025-01-09 14:40:29 -0500

    basic

--- receive_serial_data_from_arduino.py
+++ receive_serial_data_from_arduino.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 import serial
-import random
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
 
     while True:
-        number = ser.read()
-        print("number", number)
-        if number != b'':
-            if int.from_bytes(number, byteorder='big') == 18:
-                led_number = random.randint(1,4)
-                print("Button has been pressed.")
-                print("Sending number " + str(led_number) + " to Arduino.")
-                ser.write(str(led_number).encode('utf-8'))
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
