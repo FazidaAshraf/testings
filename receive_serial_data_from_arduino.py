ee19b66065b71ae63f8fe6a41cb3ee18527ead73
Fazida Ashraf Puthiyapurayil <FazidaAshraf_Puthiyapurayil@student.uml.edu>
2025-01-09 15:17:03 -0500

    another test

--- receive_serial_data_from_arduino.py
+++ receive_serial_data_from_arduino.py
@@ -5,13 +5,18 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     ser.reset_input_buffer()
 
+    array_tmp = []
+
     while True:
-        cc=str(ser.readline())
-        print("cc:",cc)
-        
-        """if ser.in_waiting > 0:
-            cc=str(ser.readline())
-            print("cc:",cc)
-            line = ser.readline().decode('utf-8').rstrip()
-            #print(line)
-            """
+        byte_read = ser.read() # Read bytes from serial port
+        print("byte_read:", byte_read)
+        if byte_read:#if not empty
+            byte_char = byte_read.decode() # Decode bytes to string
+            print("byte_char:", byte_char)
+            array_tmp.append(byte_char)
+            if byte_char == '\n':
+                print(''.join(array_tmp)) # Print the received line
+                print("array_tmp:", array_tmp) # Print the received line
+                array_tmp = []
+
+    ser.close() 
