aae049b0b1869646ecd2fd31fb695690b5912e0f
Fazida Ashraf Puthiyapurayil <FazidaAshraf_Puthiyapurayil@student.uml.edu>
2025-01-09 15:39:42 -0500

    print

--- receive_serial_data_from_arduino.py
+++ receive_serial_data_from_arduino.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
     ser.reset_input_buffer()
 
     while True:
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
